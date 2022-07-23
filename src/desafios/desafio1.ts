57b86b4cc00b184175204e91daf2f33781ed3fe4
Jefferson Braga <jeffersonbraga90@gmail.com>
2022-07-22 21:04:46 -0300

    desafio de typescript da DIO

---
+++ src/desafios/desafio1.ts
@@ -0,0 +1,10 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+interface Employee {
+  code: number,
+  name: string,
+}
+
+let employee =  {} as Employee;
+employee.code = 10;
+employee.name = "John";
